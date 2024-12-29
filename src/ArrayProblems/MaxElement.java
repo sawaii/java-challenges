ce59bcb43dacfd3c6d37b5f318bef3f3c0cbce33
Sawai <sawai.singh@zupee.in>
2024-12-30 00:34:29 +0530

    Implement MaxElement class to find the maximum value in an array

---
+++ src/ArrayProblems/MaxElement.java
@@ -0,0 +1,19 @@
+package ArrayProblems;
+
+public class MaxElement {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("The maximum element in the array is: " + maxElement(arr));
+    }
+
+    public static int maxElement(int[] arr) {
+        int max = arr[0];
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
